2c23536246336528429f37de222b6441666afc88
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-08-05 16:52:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@ const e = document.createElement('div')
 e.innerHTML='hllo there'
 e.style.color='#6666'
 document.body.appendChild(e);
+let ul=document.createElement('ul');
+let li=document.createElement('li');
+li.innerHTML='you';
+ul.appendChild(li);
+e.appendChild(ul)

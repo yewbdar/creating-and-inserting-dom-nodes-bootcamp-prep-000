955fb0dde6aa65e559a8d28ac5578ca5bf2a2c15
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-08-05 16:49:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 const e = document.createElement('div')
 e.innerHTML='hllo there'
-e.style.color='#333'
+e.style.color='#6666'
 document.body.appendChild(e);
